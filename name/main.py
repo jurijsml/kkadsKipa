987135187fa258c96623b4e34ac67b812a7408a5
jurijsml <90174881+jurijsml@users.noreply.github.com>
2021-11-30 07:19:52 +0000

    Initial commit

---
+++ name/main.py
@@ -0,0 +1,21 @@
+from PIL import Image
+
+def bilde():
+
+  datne = "name/th.jpg"
+  with Image.open(datne) as im:
+
+  
+    print(datne, im.format, f"{im.size}x{im.mode}")
+    im.show()
+
+    izmers = (100,100)
+
+    im.thumbnail(izmers)
+
+    im.save("name/wz.jpg", im.format)
+
+
+
+
+bilde()
