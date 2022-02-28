2ec0b48fc1b3883e6aeb82e1d4eaf0c0d006a30b
jurijsml <90174881+jurijsml@users.noreply.github.com>
2022-02-28 08:09:03 +0000

    l

--- flask1/flask1.py
+++ flask1/flask1.py
@@ -2,6 +2,7 @@ from flask import Flask
 import json
 
 app = Flask(__name__)
+app.config([JSON_AS_ASCII]) = False
 
 @app.route("/")
 def hello():
@@ -11,11 +12,22 @@ def hello():
 def datums():
   return "Šodien ir 15.februāris"
 
-@app.route("/lietotajs/<vards>")
-def lietotajs(vards):
-  jaunsIeraksts = {"vards":vards}
+@app.route("/lietotajs/<vards>/<vecums>")
+def lietotajs(vards,vecums):
 
-  with open("dati/lietotaji.json", "w") as f:
-    f.write(json.)
+  with open("dati/lietotaji.json", "w", encoding = "utf-8") as f:
+    json_data = json.load(f)
 
+    for key in json.data.key():
+      if key == vards:
+        break
+      if key != vards:
+        json_data[vards] = vecums
+    return f"{vards} ir sarakstā."
+     
+  with open("dati/lietotaji.json", "w", encoding = "utf-8") as f:
+    json.dump(jaunsIeraksts,f,indent=2,ensure_ascii=False)
+    
+  return json_data
+  
 app.run(host = "0.0.0.0", port=8080)
